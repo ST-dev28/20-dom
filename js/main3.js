3ec1f33831cad41d2e5a3bd675eba9d3f04af69d
SandraT <sndrbltr@gmail.com>
2021-06-04 15:50:34 +0000

    Teksto variacijos

--- js/main3.js
+++ js/main3.js
@@ -27,13 +27,16 @@ const plusDOM = document.querySelector('.plus');
 const numberDOM = document.querySelector('.number');
 const resetDOM = document.querySelector('.reset');
 const h1DOM = document.querySelector('h1');
+const gameInProgressMessage = 'Žaidimas progrese';
+const inilialH1Text = h1DOM.innerText; // galime sukurti pradini h1 teksta kaip konstanta
 
 let rezultatas = 0;
 
 minusDOM.addEventListener('click', () => {
     rezultatas = rezultatas - 1;
     numberDOM.innerText = rezultatas
-    h1DOM.innerText = 'Žaidimas progrese';
+    //h1DOM.innerText = 'Žaidimas progrese';
+    h1DOM.innerText = gameInProgressMessage;  // variantas, kai sukurem zinutes tekstui constanta
 })
 plusDOM.addEventListener('click', () => {
     rezultatas = rezultatas + 1;   // arba rezultatas += 1
@@ -44,6 +47,7 @@ resetDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas
     h1DOM.innerText = 'Te prasideda žaidynės! 🥳';
+    h1DOM.innerText = inilialH1Text;     // variantas, kai sukurem h1 tekstui constanta
 })
 
 /*  ---ARBA analogiskas variantas ---
