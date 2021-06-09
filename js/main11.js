23c45986a27e1617772dabb31097b314e22e004f
SandraT <sndrbltr@gmail.com>
2021-06-09 17:59:07 +0000

    Header menu uzduotis #16

---
+++ js/main11.js
@@ -0,0 +1,24 @@
+function renderNav() {
+
+}
+
+const menu = [
+    {
+        href: '#',
+        title: 'Home'
+    },
+    {
+        href: '#',
+        title: 'Services'
+    },
+    {
+        href: '#',
+        title: 'About us'
+    },
+    {
+        href: '#',
+        title: 'Contact us'
+    }
+];
+
+renderNav('header', menu);
