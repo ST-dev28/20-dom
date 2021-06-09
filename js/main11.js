b78db50eedd9f47b39b13de8f9b4092ffdd4ada3
SandraT <sndrbltr@gmail.com>
2021-06-09 18:44:16 +0000

    #16 solution

--- js/main11.js
+++ js/main11.js
@@ -13,11 +13,6 @@ Funkcijos uzduotis, pagal duota meniu informacijos sarasa sugeneruoti
 Visas sugeneruotas tekstinis HTML turi buti istatytas i nurodyta 
 selector'iaus elementa
 */
-
-function renderNav() {
-
-}
-
 const menu = [
     {
         href: '#',
@@ -36,5 +31,27 @@ const menu = [
         title: 'Contact us'
     }
 ];
+
+
+function renderNav(selector, list) {
+    const DOM = document.querySelector(selector);
+
+    if (!Array.isArray(list) ||
+        list.length === 0) {
+        console.error('ERROR: sarasas negali buti tuscias');
+        return false;
+    }
+
+    let HTML = '';
+    let HTML1 = '';
+    let HTML2 = '';
+    for (const item of list) {
+        HTML += `<a href="${item.href}">${item.title}</a>`;
+        HTML1 = `<nav>`
+        HTML2 = `</nav>`
+    }
+    DOM.innerHTML = HTML1 + HTML + HTML2;
+}
+
 
 renderNav('header', menu);
