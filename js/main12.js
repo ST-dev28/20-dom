bf83536a3481f2c9ce08d5e8f7dc81446c84c12e
SandraT <sndrbltr@gmail.com>
2021-06-10 17:57:12 +0000

    Select uzdavinys #17

---
+++ js/main12.js
@@ -0,0 +1,11 @@
+/*
+Reikia sugeneruoti`<select>` formos elementa, kuriame butu isvardintos
+tokios reiksmes: zuikis, barsukas, lape, vilkas, sernas, stirna, vovere.
+
+Sugeneruotas`<select>` elementas turi buti iterptas tarp formos`<label>`
+ir`<button>` elementu.
+
+Paspaudus submit mygtuka, pasirinkta`<select>` reiksme turi buti
+atvaizduota`.option` elemente.
+*/
+
