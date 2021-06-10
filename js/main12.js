12da1839d88b7eff9c82e42486bb2d3b12fce999
SandraT <sndrbltr@gmail.com>
2021-06-10 19:09:46 +0000

    #17 HTML

--- js/main12.js
+++ js/main12.js
@@ -8,4 +8,17 @@ ir`<button>` elementu.
 Paspaudus submit mygtuka, pasirinkta`<select>` reiksme turi buti
 atvaizduota`.option` elemente.
 */
+const animals = ['zuikis', 'barsukas', 'lape', 'vilkas', 'sernas', 'stirna', 'vovere'];
+function renderOption(selector, list) {
+    const DOM = document.querySelector(selector);
+    const labelDOM = document.querySelector('label');
 
+    let HTML = '';
+    for (const item of list) {
+        HTML += `<option value="${item}">${item}</option>`;
+        console.log(HTML);
+    }
+    labelDOM.insertAdjacentHTML('afterend', `<select> ${HTML} </select>`);
+}
+
+renderOption('form', animals);
